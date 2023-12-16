b1342e1de1fc3c528404d711ba97dbaebdff9a43
Janemils <143650941+Janemils@users.noreply.github.com>
2023-12-16 09:40:54 +0530

    Update K6_Script.js

--- K6_Sample_Script/K6_Script.js
+++ K6_Sample_Script/K6_Script.js
@@ -2,7 +2,7 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  let resp= http.get('https://www.google.com');
+  let resp= http.get('https://httpbin.test.k6.io/');
   // let data = { name: 'Bert' };
   // const resp=http.post('https://hkdk.events/0vgUlgs1tuph',JSON.stringify(data),{
   //   headers:{
